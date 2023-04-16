b2f9d1960a1e352b017136dd61f7b1e4eac57a8d
VaylSin <slyb33developer@gmail.com>
2023-04-17 00:13:33 +0200

    trending + api call

---
+++ src/components/Markets.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Markets = () => {
+  return (
+    <section className="mt-8">
+        <h1 className="text-2xl mb-2">Markets</h1>
+    </section>
+  )
+}
+
+export default Markets
---
+++ src/components/Trending.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import useAxios from '../hooks/useAxios'
+import CoinTrending from './CoinTrending';
+
+const Trending = () => {
+
+    const { response } = useAxios('search/trending');
+    return (
+        <div className="mt-8">
+            <h1 className='text-2xl mb-8'>Trending</h1>
+            {response && response.coins.map( coin => <CoinTrending key={coin.item.coin_id} coin={coin.item} />)}
+        </div>
+    )
+}
+
+export default Trending
