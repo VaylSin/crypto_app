d74ad359b760ab8051407003c82a2a8364620def
VaylSin <slyb33developer@gmail.com>
2023-04-17 13:12:37 +0200

    history chart

--- src/components/HistoryCharts.jsx
+++ src/components/HistoryCharts.jsx
@@ -1,22 +1,65 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import useAxios from '../hooks/useAxios';
+import moment from "moment";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
 
 const HistoryCharts = () => {
 
-    const { id } = useParams();
-    const { response } = useAxios(`coins/${id}/market_chart?vs_currency=usd&days=7`);
-
-    if(!response) {
-      return <div>Loading...</div>
-    }
-    const coinChartData = response.prices.map(value => ({
-      x:value[0],
-      y:value[0].toFixed(1)
-    }) );
-    console.log(coinChartData)
+  const { id } = useParams();
+  const { response } = useAxios(`coins/${id}/market_chart?vs_currency=usd&days=15`);
+  
+  if(!response) {
     return (
-      <div>HistoryCharts</div>
+      <div className="wrapper-container mt-8">
+        {/* <Skeleton className="h-72 w-full mb-10" /> */}
+      </div>
+    )
+  }
+  const coinChartData = response.prices.map(value => ({ x: value[0], y: value[1].toFixed(2) }));
+  
+  const options = {
+    responsive: true
+  }
+  const data = {
+    labels: coinChartData.map(value => moment(value.x).format('MMM DD')),
+    datasets: [
+      {
+        fill: true,
+        label: id,
+        data: coinChartData.map(val => val.y),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      }
+    ]
+  }
+
+    return (
+      <div>
+        <Line options={options} data={data} />
+      </div>
     )
 }
 
