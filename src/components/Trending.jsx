095edce7506e695a0bcc2307e0db10bb11444e99
SlyB33 <skvmOmega@gmail.com>
2025-02-20 15:46:16 +0100

    fix responsive display

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import useAxios from '../hooks/useAxios'
-import CoinTrending from './CoinTrending';
-import Skeleton from './Skeleton';
+import React from "react";
+import useAxios from "../hooks/useAxios";
+import CoinTrending from "./CoinTrending";
+import Skeleton from "./Skeleton";
 
 const Trending = () => {
+	const { response, loading } = useAxios("search/trending");
 
-    const { response, loading } = useAxios('search/trending');
+	if (loading) {
+		return (
+			<div className="wrapper-container">
+				<Skeleton className="h-8 w-32" />
+				<Skeleton className="h-8 w-full" mt-2 />
+				<Skeleton className="h-8 w-full" mt-2 />
+				<Skeleton className="h-8 w-full" mt-2 />
+				<Skeleton className="h-8 w-full" mt-2 />
+			</div>
+		);
+	}
+	return (
+		<div className="mt-8">
+			<h1 className="text-2xl mb-8">Les principaux cours du marché actuel</h1>
+			<div className="grid grid-cols md:grid-cols-3 xl:grid-cols-4 gap-4 p-8 lg:p-0 trends_container">
+				{response &&
+					response.coins.map((coin) => (
+						<CoinTrending key={coin.item.coin_id} coin={coin.item} />
+					))}
+			</div>
+		</div>
+	);
+};
 
-    if(loading){
-        return (
-            <div className="wrapper-container">
-                <Skeleton className="h-8 w-32" />
-                <Skeleton className="h-8 w-full" mt-2 />
-                <Skeleton className="h-8 w-full" mt-2 />
-                <Skeleton className="h-8 w-full" mt-2 />
-                <Skeleton className="h-8 w-full" mt-2 />
-            </div>
-        )
-    }
-    return (
-        <div className="mt-8">
-            <h1 className='text-2xl mb-8'>Les principaux cours du marché actuel</h1>
-            <div className="grid grid-cols-4 gap-4 trends_container">
-                {response && response.coins.map( coin => <CoinTrending key={coin.item.coin_id} coin={coin.item} />)}
-            </div>
-        </div>
-    )
-}
-
-export default Trending
+export default Trending;
